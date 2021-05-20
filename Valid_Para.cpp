4c8520a301b49585063f2cfc18718d67ffa72e8c
Kajal Jitendra Pawar <64576673+kajalp23@users.noreply.github.com>
2021-05-20 20:14:31 +0530

    Create Valid_Para.cpp

---
+++ Valid_Para.cpp
@@ -0,0 +1,30 @@
+/*
+
+Program To Find How Many Total Valid Parenthesis Sequence Will Be Formed 
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int Para(int n){
+    int cata[n+1];
+    cata[0]=1;
+    for(int i=1;i<=n;i++){
+        cata[i]=0;
+        for(int j=0;j<i;j++){
+            cata[i]+=(cata[j]*cata[i-j-1]);
+        }
+    }
+
+    return cata[n];
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << "Total Valid Parenthesis:" << Para(n);
+
+}
